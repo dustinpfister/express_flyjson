f2c3618b898fe3a04338be10960fd577a6a4c924
dustinpfister <dustin.pfister@gmail.com>
2018-06-28 16:04:43 -0400

    fixed bug that resulted in wrong timestamps

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || process.argv[2] || 8080);
 app.use('/json', require('./mw/json_fly')({
 
         //path_db: path.join(__dirname, 'db', 'days.json')
-		path_db: path.join(__dirname, 'lib','db', 'db.json')
+        path_db: path.join(__dirname, 'lib', 'db', 'db.json')
 
     }));
 
